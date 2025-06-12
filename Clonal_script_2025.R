7be4bd0a250abdb0f8fcb7c3cc65d9004f2563ec
SvandaPetr <Svandapetr@natur.cuni.cz>
2025-06-12 13:45:35 +0200

    ready

--- Clonal_script_2025.R
+++ Clonal_script_2025.R
@@ -11,17 +11,31 @@
 # 0. Install and load packages ----
 #----------------------------------------------------------#
 
-renv::install("here")
-renv::install("tidyverse")  
-renv::install("readxl")
-renv::install("usethis")
+install("here")
+install("readxl")
+install("usethis")
 
 library("here")  
-library("tidyverse")  
 library("readxl")
 library("usethis")
 
 #----------------------------------------------------------#
 # 1. Import dataset ----
 #----------------------------------------------------------#
+
+here::here("data","Rhodopea_seedlings.xlsx")
+
+korinky <- readxl::read_xlsx("data/Rhodopea_seedlings.xlsx", sheet = "harvest_data",
+                              na = c("", NA), 
+                              col_types = c("text", "numeric", "text", "numeric", "text", "text", "text", "text", "numeric", "numeric", "numeric", "numeric", "text", "numeric", "numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric", "text"),
+                              col_names = c("samle_id", "plate_id", "position_id", "row", "column", "mother_id", "mother_ploidy", "sample_ploidy", "above_RS_before", "below_RS_before", "axillary_rosettes_before", "leaves_before", "treatment", "leaves_main", "leaves_axillary", "leaves_RS", "longest_leaf", "axillary_rosettes","RS_rosettes", "root_axes_main", "below_RS_in", "below_RS_out", "weight_root_in", "weight_root_out_noSRL","weight_root_out", "weight_shoot", "weight_SRL", "root_lenght_2_m", "SRL_m_g", "notes"),
+                              skip = 1
+                             )
+
+
+#----------------------------------------------------------#
+# 2. Analyses ----
+#----------------------------------------------------------#
+
+
 
